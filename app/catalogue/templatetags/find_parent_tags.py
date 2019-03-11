ae9d89990f981773078de374a86d31c89a015a95
Igor Smurov <smurov_igor@mail.ru>
2019-03-12 00:09:30 +0300

    add photo login on detail

--- app/catalogue/templatetags/find_parent_tags.py
+++ app/catalogue/templatetags/find_parent_tags.py
@@ -5,6 +5,7 @@ from oscar.core.loading import get_model
 register = template.Library()
 
 Line = get_model('wishlists', 'Line')
+Product = get_model('catalogue', 'Product')
 
 
 @register.simple_tag
@@ -12,6 +13,15 @@ def get_product_parent(product):
     if product and product.is_child:
         return product.parent
     return product
+
+
+@register.simple_tag
+def get_all_images_for_product(product):
+    if not isinstance(product, Product):
+        return []
+    if product.is_child:
+        return (*product.get_all_images()[:3], *product.parent.get_all_images()[2:4])
+    return product.get_all_images()[2:7]
 
 
 @register.simple_tag
