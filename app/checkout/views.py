c2aa8d32a6a9839fd48e1f50a7e07d92c965cbc8
Igor Smurov <smurov_igor@mail.ru>
2019-03-15 10:59:31 +0300

    Create new branch for production

--- app/checkout/views.py
+++ app/checkout/views.py
@@ -3,8 +3,8 @@ from django.views import generic
 from oscar.apps.checkout.views import PaymentDetailsView as OscarPaymentDetailsView
 
 
-# class IndexView(generic.TemplateView):
-#     template_name = 'artskill/checkout_temp.html'
+class IndexView(generic.TemplateView):
+    template_name = 'artskill/checkout_temp.html'
 
 
 class PaymentDetailsView(OscarPaymentDetailsView):
